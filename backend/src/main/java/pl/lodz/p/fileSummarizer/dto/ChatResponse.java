df11da24a04c4404e9c10f69deebbc8155624640
236660 Strachota Piotr <236660@edu.p.lodz.pl>
2023-11-12 09:48:20 +0100

    add dto classes for openAI integration

---
+++ backend/src/main/java/pl/lodz/p/fileSummarizer/dto/ChatResponse.java
@@ -0,0 +1,16 @@
+package pl.lodz.p.fileSummarizer.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ChatResponse {
+    private List<Choice> choices;
+
+    @Data
+    public static class Choice {
+        private int index;
+        private Message message;
+    }
+}
