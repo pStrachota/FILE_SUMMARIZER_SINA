7681724725c7127614024a6319d6b22c54c8222f
236660 Strachota Piotr <236660@edu.p.lodz.pl>
2023-11-12 09:19:07 +0100

    backend initialization

---
+++ backend/src/main/java/pl/lodz/p/fileSummarizer/FileSummarizerApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.fileSummarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSummarizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileSummarizerApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/pl/lodz/p/fileSummarizer/FileSummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.fileSummarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSummarizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
